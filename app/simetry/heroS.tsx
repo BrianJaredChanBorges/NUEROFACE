62e84b5c98e059b781629b00f6228c17d1dfc9ec
brian <l23530234@cancun.tecnm.mx>
2025-10-01 14:52:36 -0500

    brian simetri image

--- app/simetry/heroS.tsx
+++ app/simetry/heroS.tsx
@@ -39,41 +39,12 @@ export const HeroSection = () => {
               <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
             </Button>
 
-           <Button
-  asChild
-  variant="secondary"
-  className="w-5/6 md:w-1/4 font-bold"
->
-  <Link href="/simetry/live" target="_blank" rel="noopener noreferrer">
-    Abrir análisis en vivo
-  </Link>
-</Button>
-
 <Button asChild className="w-5/6 md:w-1/4 font-bold">
   <Link href="/simetry/video" target="_blank" rel="noopener noreferrer">
     Analizar un video
   </Link>
 </Button>
-
           </div>
-
-        </div>
-          
-        <div className="relative group mt-14">
-          <div className="absolute top-2 lg:-top-8 left-1/2 transform -translate-x-1/2 w-[90%] mx-auto h-24 lg:h-80 bg-primary/50 rounded-full blur-3xl"></div>
-          <Image
-            width={1200}
-            height={1200}
-            className="w-full md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-secondary  border-t-primary/30"
-            src={
-              theme === "light"
-                ? "/hero-image-light.jpeg"
-                : "/hero-image-dark.jpeg"
-            }
-            alt="dashboard"
-          />
-
-          <div className="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg"></div>
         </div>
       </div>
     </section>
